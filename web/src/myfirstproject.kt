12221b403305e4887c49b6ac0883bc5483a272bd
tankaraj <H@>
2023-10-31 11:24:19 +0900

    welcome

---
+++ web/src/myfirstproject.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val add = 6 + 2
+    println(add) // 8
+
+    val minus = 6 - 2
+    println(minus) // 4
+
+    val mul = 6 * 2
+    println(mul) // 12
+
+    val divide = 6 / 2
+    println(divide) // 3
+
+    val divide2 = 7 / 2
+    println(divide2) // 3 (!)
+
+    val divide3 = 8 / 2
+    println(divide3) // 4
+
+    val divide4 = 7 / 2.0
+    println(divide4) // 3.5 (!)
+
+    val remainder = 7 % 2
+    println(remainder) // 1
+}
